41d1772dd08201e4c025a9372e2442a4e19bfec8
Mandip Das <dmandip53@gmail.com>
2026-04-21 07:32:45 +0545

    Update API baseURL to correct Render backend: rentaride-gifa.onrender.com

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend.onrender.com",
+  baseURL: "https://rentaride-gifa.onrender.com",
 });
 
 export default API;
